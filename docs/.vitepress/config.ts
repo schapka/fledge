0a093ec8f62f39ee969e2d22a823fd36f97c13f6
René Schapka <rene.schapka@gmail.com>
2024-10-23 15:50:26 +0200

    docs: fix social links

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -59,6 +59,6 @@ export default defineConfig({
       },
     ],
 
-    socialLinks: [{ icon: 'github', link: 'https://github.com/vuejs/vitepress' }],
+    socialLinks: [{ icon: 'github', link: 'https://github.com/schapka/fledge' }],
   },
 });
