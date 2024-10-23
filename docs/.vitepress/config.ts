1c7e384642209c037c0d8d0b51ccd128a8378953
René Schapka <rene.schapka@gmail.com>
2024-10-23 15:23:33 +0200

    docs: setup vitepress

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,34 @@
+import { defineConfig } from 'vitepress';
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: 'Fledge',
+  description:
+    'Streamline project scaffolding and future-proof your development with flexible, scalable templates.',
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+
+    logo: {
+      light: '/assets/fledge-black.svg',
+      dark: '/assets/fledge-white.svg',
+    },
+    siteTitle: false,
+
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' },
+    ],
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' },
+        ],
+      },
+    ],
+
+    socialLinks: [{ icon: 'github', link: 'https://github.com/vuejs/vitepress' }],
+  },
+});
---
+++ docs/index.md
@@ -0,0 +1,24 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: 'Fledge'
+  # text: 'Streamline project scaffolding and future-proof your development with flexible, scalable templates.'
+  tagline: Streamline project scaffolding and future-proof your development with flexible, scalable templates.
+  actions:
+    - theme: brand
+      text: Markdown Examples
+      link: /markdown-examples
+    - theme: alt
+      text: API Examples
+      link: /api-examples
+
+features:
+  - title: Feature A
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature B
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature C
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+---
