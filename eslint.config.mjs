aede3ade082bb743e3a7f812b2dd0ce8e95669a3
Tobias Dossinger <tobias.dossinger@googlemail.com>
2024-10-31 17:03:07 +0100

    fix: lint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,7 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   {
-    ignores: ['dist/*'],
+    ignores: ['dist/*', 'docs/*'],
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
