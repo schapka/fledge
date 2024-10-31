ef36f1e42700ea1827541609148f8dfaa868d6d2
Tobias Dossinger <tobias.dossinger@googlemail.com>
2024-10-31 21:03:52 +0100

    fix: linting

--- docs/scripts/zod-to-types.ts
+++ docs/scripts/zod-to-types.ts
@@ -1,11 +1,11 @@
 import { writeFileSync } from 'node:fs';
-import { printNode, zodToTs } from 'zod-to-ts'
+import { printNode, zodToTs } from 'zod-to-ts';
 
 // import your Zod schema
-import { configSchema } from '../../src/core/schemas.ts'
+import { configSchema } from '../../src/core/schemas.ts';
 
 // pass schema and name of type/identifier
-const { node } = zodToTs(configSchema, 'Fledge')
-const nodeString = printNode(node)
+const { node } = zodToTs(configSchema, 'Fledge');
+const nodeString = printNode(node);
 
 writeFileSync('src/exports/schema.txt', nodeString, 'utf-8');
