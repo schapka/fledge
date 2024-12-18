6f82739d932b7a553e19797e0d4c94a1266a1507
Tobias Dossinger <tobias.dossinger@googlemail.com>
2024-12-18 14:14:38 +0100

    chore: changes typing

--- docs/data/zod.data.ts
+++ docs/data/zod.data.ts
@@ -2,18 +2,18 @@ import { type ZodTypeAny } from 'zod';
 import { defineLoader } from 'vitepress';
 import { printNode, zodToTs } from 'zod-to-ts';
 
-import { configSchema, type Config } from '../../src/core/schemas.js';
+import { configSchema } from '../../src/core/schemas.js';
 
 type Data = {
   configSchema: string;
 };
 
-function convertZodSchemaToString(schema: ZodTypeAny | Config, schemaTitle: string) {
+function convertZodSchemaToString(schema: ZodTypeAny | typeof configSchema, schemaTitle: string) {
   const { node } = zodToTs(schema as ZodTypeAny, schemaTitle);
   return printNode(node);
 }
 
-const config = convertZodSchemaToString(configSchema as unknown as Config, 'Config Schema');
+const config = convertZodSchemaToString(configSchema, 'Config Schema');
 
 export default defineLoader({
   load(): Data {
